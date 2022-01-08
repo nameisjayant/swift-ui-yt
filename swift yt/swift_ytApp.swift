cdc83544c484e04b1d4ed90da842d976b7995faa
Aashish <aashishkumar12376@gmail.com>
2022-01-08 22:34:25 +0530

    Initial Commit

---
+++ swift yt/swift_ytApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ytApp.swift
+//  swift yt
+//
+//  Created by jayant kumar on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_ytApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
